21e9e3411d5b4463c714699194f1791c26b3bc48
vaansuit <vvansuit@outlook.com>
2023-04-19 22:30:32 -0300

    style: changed background image

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -17,51 +17,37 @@ class _HomePageState extends State<HomePage> {
             Container(
               decoration: const BoxDecoration(
                 image: DecorationImage(
-                  image: AssetImage('assets/background.jpg'),
+                  image: AssetImage('assets/background.gif'),
                   fit: BoxFit.fill,
                 ),
               ),
             ),
-            Positioned(
-              child: Align(
-                alignment: const Alignment(0, -0.8),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'Blumenau',
-                      style: TextStyle(
-                        fontSize: 34,
-                      ),
-                    ),
-                    const Text(
-                      '19°',
-                      style: TextStyle(
-                        fontSize: 34,
-                      ),
-                    ),
-                    const Text(
-                      'Mostly Clear',
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Text('H: 24'),
-                        Text('L: 30'),
-                      ],
-                    )
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  'Blumenau',
+                  style: TextStyle(
+                    fontSize: 34,
+                  ),
+                ),
+                const Text(
+                  '19°',
+                  style: TextStyle(
+                    fontSize: 34,
+                  ),
+                ),
+                const Text(
+                  'Mostly Clear',
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text('H: 24'),
+                    Text('L: 30'),
                   ],
-                ),
-              ),
-            ),
-            Positioned(
-              child: Align(
-                alignment: Alignment(0, 0.7),
-                child: Image.asset(
-                  'assets/house3.png',
-                  height: 200,
-                ),
-              ),
+                )
+              ],
             ),
           ],
         ),
