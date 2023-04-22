4da956b0048d69840e78016aea7bf8605155a4ab
vaansuit <vvansuit@outlook.com>
2023-04-22 11:12:37 -0300

    feat: created current and future weather models

---
+++ lib/models/current_weather_model.dart
@@ -0,0 +1,15 @@
+class CurrentWeatherModel {
+  String location;
+  double temperature;
+  String weatherCondition;
+  double latitude;
+  double longitude;
+
+  CurrentWeatherModel({
+    required this.location,
+    required this.temperature,
+    required this.weatherCondition,
+    required this.latitude,
+    required this.longitude,
+  });
+}
---
+++ lib/models/future_weather_model.dart
@@ -0,0 +1,11 @@
+class FutureWeatherModel {
+  String date;
+  String icon;
+  double temperature;
+
+  FutureWeatherModel({
+    required this.date,
+    required this.icon,
+    required this.temperature,
+  });
+}
