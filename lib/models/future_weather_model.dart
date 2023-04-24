71f9abf74a083c8694f4f97997d4d391f064ead7
vaansuit <vvansuit@outlook.com>
2023-04-24 10:36:30 -0300

    feat: converted current_weather to json

--- lib/models/future_weather_model.dart
+++ lib/models/future_weather_model.dart
@@ -1,11 +1,22 @@
 class FutureWeatherModel {
+  String location;
   String date;
   String icon;
   double temperature;
 
   FutureWeatherModel({
+    required this.location,
     required this.date,
     required this.icon,
     required this.temperature,
   });
+
+  factory FutureWeatherModel.fromJson(Map<String, dynamic> json) {
+    return FutureWeatherModel(
+      location: json[''],
+      date: json[''],
+      icon: json[''],
+      temperature: json[''],
+    );
+  }
 }
